dcc6fa5ebc61dfdba7ef129386c530e2cfdc260e
William Craig <wdcraig99@gmail.com>
2024-08-14 10:02:13 -0400

    remove some of the plugins that were too much/not being used.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -86,9 +86,9 @@ return {
   {
     'Pocco81/auto-save.nvim',
   },
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-  },
+  -- {
+  --   'jose-elias-alvarez/null-ls.nvim',
+  -- },
   {
     'MunifTanjim/prettier.nvim',
   },
@@ -198,32 +198,32 @@ return {
       },
     },
   },
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown' },
-  },
+  -- {
+  --   'iamcco/markdown-preview.nvim',
+  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --   build = 'cd app && yarn install',
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { 'markdown' }
+  --   end,
+  --   ft = { 'markdown' },
+  -- },
   {
     'mg979/vim-visual-multi',
   },
-  {
-    'ldelossa/gh.nvim',
-    dependencies = {
-      {
-        'ldelossa/litee.nvim',
-        config = function()
-          require('litee.lib').setup()
-        end,
-      },
-    },
-    config = function()
-      require('litee.gh').setup()
-    end,
-  },
+  -- {
+  --   'ldelossa/gh.nvim',
+  --   dependencies = {
+  --     {
+  --       'ldelossa/litee.nvim',
+  --       config = function()
+  --         require('litee.lib').setup()
+  --       end,
+  --     },
+  --   },
+  --   config = function()
+  --     require('litee.gh').setup()
+  --   end,
+  -- },
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
@@ -245,51 +245,51 @@ return {
   --   opts = {},
   -- },
 
-  {
-    'lewis6991/hover.nvim',
-    config = function()
-      require('hover').setup {
-        init = function()
-          -- Require providers
-          require 'hover.providers.lsp'
-          -- require('hover.providers.gh')
-          -- require('hover.providers.gh_user')
-          -- require('hover.providers.jira')
-          -- require('hover.providers.dap')
-          -- require('hover.providers.fold_preview')
-          -- require('hover.providers.diagnostic')
-          -- require('hover.providers.man')
-          -- require('hover.providers.dictionary')
-        end,
-        preview_opts = {
-          border = 'single',
-        },
-        -- Whether the contents of a currently open hover window should be moved
-        -- to a :h preview-window when pressing the hover keymap.
-        preview_window = false,
-        title = true,
-        mouse_providers = {
-          'LSP',
-        },
-        mouse_delay = 1000,
-      }
-
-      -- Setup keymaps
-      vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
-      vim.keymap.set('n', 'gK', require('hover').hover_select, { desc = 'hover.nvim (select)' })
-      vim.keymap.set('n', '<C-p>', function()
-        require('hover').hover_switch 'previous'
-      end, { desc = 'hover.nvim (previous source)' })
-      vim.keymap.set('n', '<C-n>', function()
-        require('hover').hover_switch 'next'
-      end, { desc = 'hover.nvim (next source)' })
-
-      -- Mouse support
-      vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = 'hover.nvim (mouse)' })
-      vim.o.mousemoveevent = true
-      -- end)
-    end,
-  },
+  -- {
+  --   'lewis6991/hover.nvim',
+  --   config = function()
+  --     require('hover').setup {
+  --       init = function()
+  --         -- Require providers
+  --         require 'hover.providers.lsp'
+  --         -- require('hover.providers.gh')
+  --         -- require('hover.providers.gh_user')
+  --         -- require('hover.providers.jira')
+  --         -- require('hover.providers.dap')
+  --         -- require('hover.providers.fold_preview')
+  --         -- require('hover.providers.diagnostic')
+  --         -- require('hover.providers.man')
+  --         -- require('hover.providers.dictionary')
+  --       end,
+  --       preview_opts = {
+  --         border = 'single',
+  --       },
+  --       -- Whether the contents of a currently open hover window should be moved
+  --       -- to a :h preview-window when pressing the hover keymap.
+  --       preview_window = false,
+  --       title = true,
+  --       mouse_providers = {
+  --         'LSP',
+  --       },
+  --       mouse_delay = 1000,
+  --     }
+  --
+  --     -- Setup keymaps
+  --     vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
+  --     vim.keymap.set('n', 'gK', require('hover').hover_select, { desc = 'hover.nvim (select)' })
+  --     vim.keymap.set('n', '<C-p>', function()
+  --       require('hover').hover_switch 'previous'
+  --     end, { desc = 'hover.nvim (previous source)' })
+  --     vim.keymap.set('n', '<C-n>', function()
+  --       require('hover').hover_switch 'next'
+  --     end, { desc = 'hover.nvim (next source)' })
+  --
+  --     -- Mouse support
+  --     vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = 'hover.nvim (mouse)' })
+  --     vim.o.mousemoveevent = true
+  --     -- end)
+  --   end,
+  -- },
   {
     'VidocqH/lsp-lens.nvim',
   },
@@ -441,14 +441,14 @@ ________________________________________________________________________________
     event = 'VeryLazy',
     enabled = vim.fn.has 'nvim-0.10.0' == 1,
   },
-  { 'artemave/workspace-diagnostics.nvim' },
+  -- { 'artemave/workspace-diagnostics.nvim' },
 
-  {
-    'nvimtools/none-ls.nvim',
-  },
-  { 'dmmulroy/tsc.nvim' },
+  -- {
+  --   'nvimtools/none-ls.nvim',
+  -- },
+  -- { 'dmmulroy/tsc.nvim' },
   { 'akinsho/toggleterm.nvim', version = '*', config = true },
-  { 'gorbit99/codewindow.nvim' },
+  -- { 'gorbit99/codewindow.nvim' },
   { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
   { 'moll/vim-bbye' },
   -- { 'yioneko/nvim-vtsls'},
@@ -462,34 +462,34 @@ ________________________________________________________________________________
   {
     'petertriho/nvim-scrollbar',
   },
-  {
-    'nvim-neotest/neotest',
-    dependencies = {
-      'nvim-neotest/nvim-nio',
-      'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-  },
-  {
-    'nvim-neotest/neotest',
-    dependencies = {
-      'thenbe/neotest-playwright',
-      dependencies = 'nvim-telescope/telescope.nvim',
-    },
-    config = function()
-      require('neotest').setup {
-        adapters = {
-          require('neotest-playwright').adapter {
-            options = {
-              persist_project_selection = true,
-              enable_dynamic_test_discovery = true,
-            },
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   'nvim-neotest/neotest',
+  --   dependencies = {
+  --     'nvim-neotest/nvim-nio',
+  --     'nvim-lua/plenary.nvim',
+  --     'antoinemadec/FixCursorHold.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --   },
+  -- },
+  -- {
+  --   'nvim-neotest/neotest',
+  --   dependencies = {
+  --     'thenbe/neotest-playwright',
+  --     dependencies = 'nvim-telescope/telescope.nvim',
+  --   },
+  --   config = function()
+  --     require('neotest').setup {
+  --       adapters = {
+  --         require('neotest-playwright').adapter {
+  --           options = {
+  --             persist_project_selection = true,
+  --             enable_dynamic_test_discovery = true,
+  --           },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     'MysticalDevil/inlay-hints.nvim',
     event = 'LspAttach',
