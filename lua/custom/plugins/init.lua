9662375eeffd20d7999e9c276054d071fc3243de
William Craig <wdcraig99@gmail.com>
2024-09-04 14:44:56 -0400

    markdown previewing

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -198,15 +198,7 @@ return {
       },
     },
   },
-  -- {
-  --   'iamcco/markdown-preview.nvim',
-  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  --   build = 'cd app && yarn install',
-  --   init = function()
-  --     vim.g.mkdp_filetypes = { 'markdown' }
-  --   end,
-  --   ft = { 'markdown' },
-  -- },
+  
   {
     'mg979/vim-visual-multi',
   },
@@ -499,19 +491,22 @@ ________________________________________________________________________________
     end,
   },
 
-  -- {
-  --   'MeanderingProgrammer/markdown.nvim',
-  --   main = 'render-markdown',
-  --   opts = {},
-  --   name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  -- },
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    main = 'render-markdown',
+    opts = {},
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
   {
     'stevearc/quicker.nvim',
     ---@module "quicker"
     ---@type quicker.SetupOptions
     opts = {},
   },
+  {
+    'honza/vim-snippets'
+  }
 }
